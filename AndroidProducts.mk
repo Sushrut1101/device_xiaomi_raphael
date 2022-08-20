acb9f0b25f073ec34bd6ac56af11262957a20387
Bauh Yeoj <Devolutiony@yandex.com>
2022-08-20 08:51:50 +0000

    raphael: Inital device tree
    took me 10 years to make this fantastic buggless tree
    @Stallix is a n00b

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/devolution_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    devolution_raphael-eng
---
+++ devolution_raphael.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from raphael device
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some common Devolution X stuff.
+$(call inherit-product, vendor/devolution/config/common_full_phone.mk)
+DEVO_BUILD_TYPE := UNOFFICIAL
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := devolution_raphael
+PRODUCT_DEVICE := raphael
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
